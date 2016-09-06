01c7a57dc69cbae0c403133c9c43ffea6fd9d8db
S0o <s0ospes@gmail.com>
2016-09-06 17:00:50 +0300

    Micromir. Добавлена динамическая масштабная риска. Экраны выбора динамически расширяемы, в зависимости от количества кнопок

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -1,18 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Scale : MonoBehaviour {
 
     public float scale;
+    public float um500=311.5f;
     public ViewerZoom vieverScript;
-	// Use this for initialization
-	void Start () {
+    public float units;
+    public string nano= "nm";
+    public string micro= "μm";
+    public Text unitsNumb;
+    public Text unitsText;
+
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
         scale = vieverScript.scale;
-        transform.localScale = new Vector3(scale, 1, 1);
+        if (scale <=2)
+        {
+            units = um500;
+            unitsNumb.text = "500";
+            unitsText.text = micro;
+        }
+        else
+         if (scale<=4)
+         {
+             units = 2*um500/5;
+             unitsNumb.text = "200";
+             unitsText.text = micro;
+        }
+        else
+         if (scale <= 8)
+        {
+            units = um500/5;
+            unitsNumb.text = "100";
+            unitsText.text = micro;
+        }
+        else
+         if (scale <= 16)
+        {
+            units = um500/10;
+            unitsNumb.text = "50";
+            unitsText.text = micro;
+        }
+       
+       
+        transform.GetComponent<RectTransform>().sizeDelta = new Vector2(scale*units,12f);
     }
 }
